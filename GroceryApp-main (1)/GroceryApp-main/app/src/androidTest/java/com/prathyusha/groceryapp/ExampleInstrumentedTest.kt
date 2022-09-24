a295cf69a8b567d7339404f412689788aba57925
prathyusha251 <114305476+prathyusha251@users.noreply.github.com>
2022-09-24 22:29:32 +0530

    Update ExampleInstrumentedTest.kt

--- GroceryApp-main (1)/GroceryApp-main/app/src/androidTest/java/com/prathyusha/groceryapp/ExampleInstrumentedTest.kt
+++ GroceryApp-main (1)/GroceryApp-main/app/src/androidTest/java/com/prathyusha/groceryapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.divyanshu.groceryapp
+package com.prathyusha.groceryapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
